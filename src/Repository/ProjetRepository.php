be12e1f23c834d373dba636d6b9ef01279657878
daniel <daniel@daniel-Inspiron-7786>
2023-03-15 09:15:57 +0100

    ajout de la donnée liée aux projets

--- src/Repository/ProjetRepository.php
+++ src/Repository/ProjetRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repository;
 
-use App\Entity\Projet;
+use App\Entity\Project;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Projet>
+ * @extends ServiceEntityRepository<Project>
  *
- * @method Projet|null find($id, $lockMode = null, $lockVersion = null)
- * @method Projet|null findOneBy(array $criteria, array $orderBy = null)
- * @method Projet[]    findAll()
- * @method Projet[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Project|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Project|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Project[]    findAll()
+ * @method Project[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ProjetRepository extends ServiceEntityRepository
+class ProjectRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Projet::class);
+        parent::__construct($registry, Project::class);
     }
 
-    public function add(Projet $entity, bool $flush = false): void
+    public function add(Project $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class ProjetRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Projet $entity, bool $flush = false): void
+    public function remove(Project $entity, bool $flush = false): void
     {
         $this->getEntityManager()->remove($entity);
 
@@ -40,7 +40,7 @@ class ProjetRepository extends ServiceEntityRepository
     }
 
 //    /**
-//     * @return Projet[] Returns an array of Projet objects
+//     * @return Project[] Returns an array of Project objects
 //     */
 //    public function findByExampleField($value): array
 //    {
@@ -54,7 +54,7 @@ class ProjetRepository extends ServiceEntityRepository
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Projet
+//    public function findOneBySomeField($value): ?Project
 //    {
 //        return $this->createQueryBuilder('p')
 //            ->andWhere('p.exampleField = :val')
